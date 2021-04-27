11024fae59eb1a34bf1cfb245f5a26ee99b2c6f8
SSH1997 <akasiassh@naver.com>
2021-04-27 17:11:37 +0900

    [BE]:[FEAT] Mysql 모듈 설치 및 db 설정

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  user: 'root',
+  password: '1234',
+  database: 'ddb',
+  host: 'localhost',
+  port: 3306
+});
 
 app.get('/test', (req, res) => {
-  res.send({test: 'success'});
+  connection.query('SELECT * FROM test',(err, results, fields) => {
+    const result = results[0];
+
+    res.send(result);
+  });
 });
 
 app.listen(port, () => {
